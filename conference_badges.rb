71d2b559ef53822028346316a65b26840cfb322e
Veronica Bustabad <vbustabad@gmail.com>
2018-01-26 22:05:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,6 @@ def assign_rooms(attendees)
 end
 
 def printer
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  batch_badge_creator(attendees)
+  assign_rooms(attendees)
 end
